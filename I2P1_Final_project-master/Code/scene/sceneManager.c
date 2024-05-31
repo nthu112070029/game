b8de87970adf60780b33c802fddf45f83186080c
nthu112070029 <janeugin@gmail.com>
2024-05-31 17:28:57 +0800

    lose 畫面

--- I2P1_Final_project-master/Code/scene/sceneManager.c
+++ I2P1_Final_project-master/Code/scene/sceneManager.c
@@ -1,9 +1,12 @@
 #include "sceneManager.h"
 #include "menu.h"
+
+#include "Lose.h"
 #include "gamescene.h"
 Scene *scene = NULL;
 void create_scene(SceneType type)
 {
+    
     switch (type)
     {
     case Menu_L:
@@ -12,6 +15,10 @@ void create_scene(SceneType type)
     case GameScene_L:
         scene = New_GameScene(GameScene_L);
         break;
+     case Lose_L:
+        scene = New_Lose(Lose_L);
+       
+        break;    
     default:
         break;
     }
