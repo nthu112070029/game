3a958027ed4ffa6998e4bfdff45e7cef13328c13
bkg547 <bkg547@gmail.com>
2024-06-01 23:34:40 +0800

    update lose win sound

--- I2P1_Final_project-master/Code/scene/Lose.c
+++ I2P1_Final_project-master/Code/scene/Lose.c
@@ -11,7 +11,7 @@ Scene *New_Lose(int label)
     
     pDerivedObj->font = al_load_ttf_font("assets/font/pirulen.ttf", 12, 0);
     // Load sound
-    pDerivedObj->song = al_load_sample("assets/sound/Lose.mp3");
+    pDerivedObj->song = al_load_sample("assets/sound/lose.mp3");
     al_reserve_samples(20);
     pDerivedObj->sample_instance = al_create_sample_instance(pDerivedObj->song);
     pDerivedObj->title_x = WIDTH / 2;
